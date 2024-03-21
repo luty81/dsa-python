830ec49ef36563cdf368cab5ba5103ccee4a9abd
Luciano Fernandes <luty@ufrj.br>
2024-03-21 15:35:37 -0300

    Added recursively insert to bst

--- dsa/trees/tree_node.py
+++ dsa/trees/tree_node.py
@@ -3,6 +3,9 @@ class TreeNode:
         self.data = data
         self.left = None
         self.right = None
+
+    def is_leaf(self):
+        return self.left is None and self.right is None
     
 
 class BinarySearchTree:
@@ -33,12 +36,6 @@ class BinarySearchTree:
                 
                 current_node = current_node.right
 
-    def r_insert(self, data):
-        if self.root is None:
-            self.root = TreeNode(data)
-            
-        return self.__r_insert(self.root, data)
-
     def find(self, value) -> TreeNode | None:
         current = self.root
         while current:
@@ -48,10 +45,23 @@ class BinarySearchTree:
             current = current.left if value < current.data else current.right
 
         return None
-                
+    
+    def r_insert(self, data):
+        if self.root is None:
+            self.root = TreeNode(data)
+            
+        return self.__r_insert(self.root, data)
+
     def r_find(self, value) -> TreeNode | None:
         return self.__r_find(self.root, value) if self.root is not None else None
+
+    def to_array(self):
+        if self.root is None:
+            return self.root
         
+        return [self.root.data] + self.__to_heap_array(self.root)
+
+
 
     def __r_find(self, node: TreeNode, value): 
         if node.data == value:
@@ -83,10 +93,19 @@ class BinarySearchTree:
             
             return self.__r_insert(node.right, data)
 
+    def __to_heap_array(self, node: TreeNode, result=None):     
+        if result is None:
+            result = []
 
-
+        if node is None or node.is_leaf():
+            return result
         
 
+        result.append(node.left.data if node.left else None)
+        result.append(node.right.data if node.right else None)
+        self.__to_heap_array(node.left, result)
+        self.__to_heap_array(node.right, result)
+        return result
 
 
 
